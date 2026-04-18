d9bb87b4a3ac7d9c3ce2e7e94dcadb22442dd7cb
anusha123-prog <peddireddyanusha162@gmail.com>
2026-04-18 23:42:00 +0530

    added some code

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ num = 8
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
